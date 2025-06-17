005bba98cba6c1eba0b18282c5eb31cc11daa450
awtism-manifesto <awpower005@gmail.com>
2025-06-17 09:48:04 -0400

    Update PlutoniumShank.cs

--- CrossMod/gunrightsmod/gunrightsmodItems/PlutoniumShank.cs
+++ CrossMod/gunrightsmod/gunrightsmodItems/PlutoniumShank.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
-using Terbritish2.Content.Projectiles;
+using Terbritish.Content.Projectiles;
 
 using Terraria;
 using Terraria.DataStructures;
