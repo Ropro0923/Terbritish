26f4f527d399dbc006f31235d65adad92e1e134b
awtism-manifesto <awpower005@gmail.com>
2025-06-16 22:49:18 -0400

    added copper, tin and uranium knives

---
+++ Content/Projectiles/UraniumKnifeThrown.cs
@@ -0,0 +1,44 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terbritish2.Content.DamageClasses;
+using Terraria.Audio;
+
+
+namespace Terbritish2.Content.Projectiles
+{
+    public class UraniumKnifeThrown : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 30;
+            Projectile.height = 30;
+            Projectile.friendly = true;
+            Projectile.DamageType = ModContent.GetInstance<britishDamage>();
+            Projectile.penetrate = 3;
+            Projectile.timeLeft = 600;
+            Projectile.tileCollide = true;
+            Projectile.ignoreWater = false;
+            Projectile.extraUpdates = 1;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 15;
+        }
+
+        public override void AI()
+        {
+
+            Projectile.rotation += 0.135f;
+            Projectile.ai[0] += 1f;
+            if (Projectile.ai[0] >= 25f)
+            {
+                Projectile.ai[0] = 25f;
+                Projectile.velocity.Y += 0.165f;
+            }
+            if (Projectile.velocity.Y > 15f)
+            {
+                Projectile.velocity.Y = 17f;
+            }
+        }
+    }
+}
