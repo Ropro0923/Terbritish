794c2e035689657eb737ff38c89dbcdfc3b9b7cb
awtism-manifesto <awpower005@gmail.com>
2025-06-16 21:03:16 -0400

    Update LondonProjectileThrown.cs
    Fixed low velocity

--- Content/Projectiles/LondonProjectileThrown.cs
+++ Content/Projectiles/LondonProjectileThrown.cs
@@ -20,21 +20,22 @@ namespace Terbritish.Content.Projectiles
             Projectile.timeLeft = 600;
             Projectile.tileCollide = true;
             Projectile.ignoreWater = false;
+            Projectile.extraUpdates = 1;
         }
 
         public override void AI()
         {
 
-            Projectile.rotation += 0.05f;
+            Projectile.rotation += 0.115f;
             Projectile.ai[0] += 1f;
             if (Projectile.ai[0] >= 25f)
             {
-                Projectile.ai[0] = 25f;
-                Projectile.velocity.Y += 0.2f;
+            Projectile.ai[0] = 25f;
+            Projectile.velocity.Y += 0.175f;
             }
             if (Projectile.velocity.Y > 15f)
             {
-                Projectile.velocity.Y = 17f;
+            Projectile.velocity.Y = 17f;
             }
         }
     }
