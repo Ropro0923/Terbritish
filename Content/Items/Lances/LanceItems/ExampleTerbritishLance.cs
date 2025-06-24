630dca4e8b0fb08700ddfded78d900162f84a9bf
Ropro0923 <rcberry2010@gmail.com>
2025-06-24 16:34:33 -0500

    Added buggy ahh lance. No clue how to do it. Added proper icon.

--- Content/Items/Lances/LanceItems/ExampleTerbritishLance.cs
+++ Content/Items/Lances/LanceItems/ExampleTerbritishLance.cs
@@ -10,6 +10,12 @@ namespace Terbritish.Content.Items.Lances.LanceItems
 {
     public class ExampleTerbritishLance : ModItem
     {
+        private int AfterSpeedTimer = 0;
+        private int AfterSpeedBoost = 0;
+        private int InitialSpeedTimer = 0;
+        private int InitialSpeedBoost = 0;
+        private int SpeedTimer = 0;
+
         public override void SetDefaults()
         {
             Item.DefaultToSpear(ModContent.ProjectileType<ExampleTerbritishLanceProjectile>(), 1f, 24);
@@ -24,16 +30,55 @@ namespace Terbritish.Content.Items.Lances.LanceItems
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            player.moveSpeed += 1f;
             float speed = velocity.Length();
             velocity = new Vector2(player.direction * speed, 0f);
             position += new Vector2(player.direction * 40f, 0f);
             Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
             return false;
         }
+
         public override void HoldItem(Player player)
         {
             player.itemRotation = 0f;
+
+            if (player.channel && player.HeldItem == Item)
+            {
+                if (InitialSpeedBoost < 10)
+                {
+                    InitialSpeedTimer++;
+                    if (InitialSpeedTimer >= 6)
+                    {
+                        InitialSpeedBoost++;
+                        InitialSpeedTimer = 0;
+                    }
+                }
+                else // InitialSpeedBoost == 10
+                {
+                    AfterSpeedTimer++;
+                    if (AfterSpeedTimer >= 30)
+                    {
+                        AfterSpeedBoost++;
+                        AfterSpeedTimer = 0;
+                    }
+                }
+                float totalMphBoost = InitialSpeedBoost + AfterSpeedBoost;
+                float pixelPerFrameBoost = totalMphBoost * 0.195f;
+
+                SpeedTimer++;
+                if (SpeedTimer >= 60)
+                {
+                    player.velocity.X += pixelPerFrameBoost * player.direction;
+                    SpeedTimer = 0;
+                }
+            }
+            else
+            {
+                InitialSpeedBoost = 0;
+                InitialSpeedTimer = 0;
+                AfterSpeedBoost = 0;
+                AfterSpeedTimer = 0;
+                SpeedTimer = 0;
+            }
         }
     }
 }
